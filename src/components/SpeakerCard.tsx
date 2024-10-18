f4888300543a6f4b12bfd7c929df4c8c00031a8f
Nick Forbes-Smith <nick@forbes-smith.com>
2024-10-18 12:43:52 +1100

    Refactor SimpleModal component + add sponsor description modal

--- src/components/SpeakerCard.tsx
+++ src/components/SpeakerCard.tsx
@@ -1,6 +1,5 @@
-import Markdown from "markdown-to-jsx";
-import React, { useEffect, useRef, useState } from "react";
-import { markdownCommonStyles } from "../utils/markdownCommonStyles";
+import React, { useEffect, useState } from "react";
+import { SimpleModal } from "./SimpleModal";
 
 type SpeakerCardProps = {
   imgSrc: string;
@@ -27,7 +26,6 @@ const SpeakerCard: React.FC<SpeakerCardProps> = ({
   shortDescription,
   longDescriptionMd,
 }) => {
-  const containerRef = useRef<HTMLDivElement>(null);
   const [modelOpen, setModelOpen] = useState(false);
   const [mddText, setMddText] = useState("");
   // Fetch Terms of Use
@@ -37,15 +35,6 @@ const SpeakerCard: React.FC<SpeakerCardProps> = ({
       .then((text) => setMddText(text))
       .catch((error) => console.error(error));
   }, [longDescriptionMd]);
-
-  useEffect(() => {
-    if (modelOpen) {
-      document.body.style.overflow = "hidden";
-      containerRef.current?.scrollTo(0, 0);
-    } else {
-      document.body.style.overflow = "auto";
-    }
-  }, [modelOpen]);
 
   return (
     <>
@@ -69,92 +58,18 @@ const SpeakerCard: React.FC<SpeakerCardProps> = ({
           <span className="text-sm text-gray-200">{shortDescription}</span>
         </div>
       </div>
-
-      <div
-        className="fixed top-0 left-0 p-2 items-center justify-center overflow-hidden z-40 bg-white bg-opacity-60 h-screen w-screen"
-        aria-labelledby="modal-title"
-        role="dialog"
-        aria-modal="true"
-        style={{
-          transition: "opacity .15s ease-in-out",
-          display: modelOpen ? "flex" : "none",
-          opacity: modelOpen ? 1 : 0,
+      <SimpleModal
+        {...{
+          mddText,
+          title,
+          open: modelOpen,
+          setOpen: setModelOpen,
+          imgSrc,
+          imgHeight: imgExpandedHeight,
+          imgPositionX: imgExpandedPositionX,
+          imgPositionY: imgExpandedPositionY,
         }}
-        onClick={() => setModelOpen(!modelOpen)}
-      >
-        <div className="flex items-center justify-center text-center">
-          <div
-            ref={containerRef}
-            style={{ flex: "1 1 auto", maxWidth: "800px" }}
-            className="relative p-4 overflow-scroll text-left align-bottom transition-all transform rounded-lg shadow-xl bg-gray-900 sm:align-middle sm:max-w-md sm:w-full sm:p-6 h-full max-h-[calc(100vh-1rem)]"
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
-          >
-            <div className="flex flex-wrap justify-center">
-              <div className="md:w-1/3 max-h-96">
-                <img
-                  style={{
-                    height: imgExpandedHeight,
-                    width: "250px",
-                    objectPosition: `${imgExpandedPositionX} ${imgExpandedPositionY}`,
-                  }}
-                  className="object-cover rounded-md"
-                  src={imgSrc}
-                  alt=""
-                />
-              </div>
-
-              <div className="md:w-2/3 mt-8 md:mt-0 px-4 flex flex-wrap">
-                <h3
-                  className="font-medium leading-6 capitalize text-white text-2xl lg:text-3xl"
-                  id="modal-title"
-                >
-                  {title}
-                </h3>
-
-                <div className="mx-auto mt-8 prose-base max-w-none">
-                  <Markdown
-                    options={{
-                      overrides: {
-                        ...markdownCommonStyles,
-                        p: { props: { className: "text-gray-200 mt-0" } },
-                      },
-                    }}
-                    children={mddText}
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="mt-5 sm:flex sm:items-center sm:-mx-2">
-              <button
-                onClick={() => setModelOpen(!modelOpen)}
-                className="w-full px-4 py-2 text-sm font-medium tracking-wide capitalize transition-colors duration-300 transform border rounded-md mb-16 sm:mb-0 sm:mx-2 text-gray-200 border-gray-700 hover:bg-gray-800 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-40"
-              >
-                Close
-              </button>
-              <button
-                onClick={() => setModelOpen(!modelOpen)}
-                className="absolute right-4 top-4 w-4 h-4 transition-colors rounded duration-300 transform text-gray-400 hover:text-gray-700 focus:outline-none focus:ring focus:ring-gray-700 focus:ring-opacity-40"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 8 8">
-                  <title>Shape + Shape</title>
-                  <g fill-rule="evenodd">
-                    <path
-                      d="M2.048.77l5.18 5.182L5.953 7.23.77 2.048 2.048.77z"
-                      fill="currentColor"
-                    />
-                    <path
-                      d="M5.952.77L7.23 2.05 2.048 7.23.77 5.952 5.953.772z"
-                      fill="currentColor"
-                    />
-                  </g>
-                </svg>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      />
     </>
   );
 };
--- src/components/SponsorshipLevel.tsx
+++ src/components/SponsorshipLevel.tsx
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { SimpleModal } from "./SimpleModal";
+
+interface SponsorCardProps {
+  name: string;
+  link: string;
+  imgSrc: string;
+  description?: string;
+}
 
 type SponsorshipLevelProps = {
   title: string;
-  cards: {
-    name: string;
-    link: string;
-    imgSrc: string;
-  }[];
+  cards: SponsorCardProps[];
+};
+
+const SponsorCard: React.FC<SponsorCardProps> = ({
+  name,
+  link,
+  imgSrc,
+  description,
+}: SponsorCardProps) => {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <div className="relative w-64 h-40 sm:w-52 sm:h-32 m-2.5">
+      {description && (
+        <SimpleModal
+          open={showModal}
+          setOpen={setShowModal}
+          title={name}
+          mddText={description}
+          imgSrc={imgSrc}
+          imgHref={link}
+          light
+        />
+      )}
+      <a
+        href={description ? "#terria" : link}
+        onClick={(evt) => {
+          if (description) {
+            setShowModal(true);
+            evt.preventDefault();
+          }
+        }}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex cursor-pointer w-full h-full bg-white rounded overflow-hidden hover:scale-105 sponsor-card justify-center"
+      >
+        {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
+        <img
+          src={imgSrc}
+          alt={name}
+          className="p-3 max-w-full max-h-full object-contain"
+        />
+      </a>
+    </div>
+  );
 };
 
 const SponsorshipLevel: React.FC<SponsorshipLevelProps> = ({
@@ -21,20 +68,7 @@ const SponsorshipLevel: React.FC<SponsorshipLevelProps> = ({
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mb-5  justify-items-center sm:justify-items-normal">
         {cards.map((card, index) => (
-          <a
-            key={index}
-            href={card.link}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex cursor-pointer w-64 h-40 sm:w-52 sm:h-32 bg-white rounded overflow-hidden hover:scale-105 m-2.5 sponsor-card justify-center"
-          >
-            {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
-            <img
-              src={card.imgSrc}
-              alt={card.name}
-              className="p-3 max-w-full max-h-full object-contain"
-            />
-          </a>
+          <SponsorCard {...card} key={index} />
         ))}
       </div>
     </>
