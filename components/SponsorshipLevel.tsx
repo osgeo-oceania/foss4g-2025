a538b721f6a63e4a78ee6c24f3478513d9786253
Nick Forbes-Smith <nick@forbes-smith.com>
2025-03-20 12:47:09 +1100

    Add first sponsors

--- components/SponsorshipLevel.tsx
+++ components/SponsorshipLevel.tsx
@@ -1,29 +1,33 @@
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import Image from "next/image";
+import Link from "next/link";
 import React, { useState } from "react";
-import { SimpleModal } from "./SimpleModal";
+// import { SimpleModal } from "../2024-site-to-be-merged-in/components/SimpleModal";
 
 export interface SponsorCardProps {
   name: string;
   link: string;
-  imgSrc: string;
+  image: StaticImport;
   description?: string;
 }
 
 type SponsorshipLevelProps = {
   title: string;
-  description?: string;
+  // Note description not supported until SimpleModal is implemented
+  // description?: string;
   cards: SponsorCardProps[];
 };
 
 export const SponsorCard: React.FC<SponsorCardProps> = ({
   name,
   link,
-  imgSrc,
+  image,
   description,
 }: SponsorCardProps) => {
   const [showModal, setShowModal] = useState(false);
   return (
     <div className="relative w-64 h-40 sm:w-52 sm:h-32 m-2.5">
-      {description && (
+      {/* {description && (
         <SimpleModal
           open={showModal}
           setOpen={setShowModal}
@@ -33,8 +37,8 @@ export const SponsorCard: React.FC<SponsorCardProps> = ({
           imgHref={link}
           light
         />
-      )}
-      <a
+      )} */}
+      <Link
         href={description ? "#info" : link}
         onClick={(evt) => {
           if (description) {
@@ -44,35 +48,33 @@ export const SponsorCard: React.FC<SponsorCardProps> = ({
         }}
         target="_blank"
         rel="noopener noreferrer"
-        className="flex cursor-pointer w-full h-full bgbg-slate-50 rounded overflow-hidden hover:scale-105 sponsor-card justify-center"
+        className="flex cursor-pointer w-full h-full bg-white rounded overflow-hidden hover:scale-105 sponsor-card justify-center"
       >
         {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
-        <img
-          src={imgSrc}
+        <Image
+          src={image}
           alt={name}
           className="p-3 max-w-full max-h-full object-contain m-0"
         />
-      </a>
+      </Link>
     </div>
   );
 };
 
 const SponsorshipLevel: React.FC<SponsorshipLevelProps> = ({
   title,
-  description,
+  // description,
   cards,
 }) => {
   return (
     <>
-      <h2 className="text-l font-medium text-gray-800 md:text-xl lg:text-2xl mb-5">
-        {title}
-      </h2>
+      <h2>{title}</h2>
 
-      {description && (
+      {/* {description && (
         <p className="text-sm text-gray-600 mb-5">{description}</p>
-      )}
+      )} */}
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mb-5  justify-items-center sm:justify-items-normal">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 mb-5 justify-items-center sm:justify-items-normal">
         {cards.map((card, index) => (
           <SponsorCard {...card} key={index} />
         ))}
