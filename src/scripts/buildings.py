ba17170627742187ba65cd08b1fafde0a0a37bdf
rami-dv <rami.dv99@gmail.com>
2025-05-26 23:06:39 -0400

    buildings preprocessing

--- src/scripts/buildings.py
+++ src/scripts/buildings.py
@@ -8,12 +8,10 @@
 # ///
 
 import geopandas as gpd
-import typer
-
-
 from pathlib import Path
-import shpyx
 import typer
+import shpyx
+import json
 
 BOUNDS_PATH = Path("src/data/bounds.geojson")
 TMP_DATA_DIR = Path("src/data/tmp/")
@@ -33,25 +31,60 @@ def build(gdal_path: str = "gdal"):
     ## build clipped & negative buffered buildings
     AUCKLAND_BUILDINGS = TMP_DATA_DIR / "auckland-buildings.gpkg"
 
+    if not AUCKLAND_BUILDINGS.exists():
+        cmd = [
+            f"{gdal_path} vector pipeline --progress",
+            # read from inside zip file
+            f'read --input "/vsizip/{BUILDINGS_PATH}/nz-building-outlines.gpkg"',
+            # clip the source input to features that intersect buildings bounds
+            f'clip --like "{BOUNDS_PATH}" --like-where "name=\'buildings\'"',
+            # negative buffer 0.00002 deg (aprox 2m)
+            "geom buffer --distance=-0.00002 --endcap-style=flat --join-style=mitre",
+            # filter out null geometries
+            'filter --where "OGR_GEOMETRY IS NOT NULL"',
+            # make sure all output is multi polygon (some negative buffers result in >1 polygon)
+            "geom set-type --multi",
+            # write as gpkg
+            f"write --overwrite --output-format=GPKG {AUCKLAND_BUILDINGS}",
+        ]
+
+        shpyx.run(" ! ".join(cmd), log_cmd=True, log_output=True)
+
+    ## height calculation dem-dsm
+    HEIGHT_RASTER = TMP_DATA_DIR / "height.tif"
+
+    # build vrt for dem and dsm separately
+    vrt_paths = []
+
+    for demdsm in DEMDSM_DIR.glob("*/"):
+        model = "dem" if "dem" in demdsm.stem else "dsm"
+        tifs = list(demdsm.glob("**/*.tiff"))
+        vrt_path = TMP_DATA_DIR / f"{model}.vrt"
+
+        cmd = [
+            f"{gdal_path} raster mosaic",
+            "--overwrite",
+            "--resolution highest",
+            " ".join([str(tif) for tif in tifs]),
+            str(vrt_path),
+        ]
+
+        shpyx.run(" ".join(cmd), log_cmd=True, log_output=True)
+        vrt_paths.append(vrt_path)
+
+    # stack and clip rasters, save output
     cmd = [
-        f"{gdal_path} vector pipeline --progress",
-        # read from inside zip file
-        f'read --input "/vsizip/{BUILDINGS_PATH}/nz-building-outlines.gpkg"',
-        # clip the source input to features that intersect buildings bounds
-        f'clip --like "{BOUNDS_PATH}" --like-where "name=\'buildings\'"',
-        # negative buffer 0.00002 deg (aprox 2m)
-        "geom buffer --distance=-0.00002 --endcap-style=flat --join-style=mitre",
-        # filter out null geometries
-        'filter --where "OGR_GEOMETRY IS NOT NULL"',
-        # make sure all output is multi polygon (some negative buffers result in >1 polygon)
-        "geom set-type --multi",
-        # write as gpkg
-        f"write --overwrite --output-format=GPKG {AUCKLAND_BUILDINGS}",
+        f"{gdal_path} raster stack",
+        "--overwrite",
+        "--output-format=GTiff",
+        "--creation-option COMPRESS=DEFLATE",
+        "--creation-option PREDICTOR=3",
+        "--bbox=1750497,5909801,1769263,5922342",  # TODO auto calculate
+        " ".join([str(vrt_path) for vrt_path in vrt_paths]),
+        str(HEIGHT_RASTER),
     ]
 
-    shpyx.run(" ! ".join(cmd), log_cmd=True, log_output=True)
-
-    pass
+    shpyx.run(" ".join(cmd), log_cmd=True, log_output=True)
 
 
 @app.command()
