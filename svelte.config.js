2264ba653f250ed435159f26185fc7ec83e7b585
rami-dv <rami.dv99@gmail.com>
2025-06-10 19:43:00 -0400

    use absolute layout path to avoid relative path issues

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
+import { join } from 'path';
 
 const config = {
   preprocess: [
     vitePreprocess(),
     mdsvex({
       extensions: ['.svx'],
-      layout: './src/lib/svx-layout.svelte'
+      layout: join(process.cwd(), './src/lib/svx-layout.svelte')
     })
   ],
   extensions: ['.svelte', '.svx'],
