7eaac47d133bc83d623849fcd4971d30b868ac17
rami-dv <rami.dv99@gmail.com>
2025-06-11 11:08:40 -0400

    link comment

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -1,4 +1,10 @@
 <script lang="ts">
+  //
+  // this is a custom <a> component to prepend the link with the base path
+  // (useful for when app is deployed on github pages with a subfolder ie
+  // osgeo-oceania.github.io/foss4g-2025/)
+  //
+
   import { PUBLIC_BASE_PATH } from '$env/static/public';
   const { href: origHref, children, ...props } = $props();
 
