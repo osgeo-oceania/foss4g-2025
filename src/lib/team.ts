4be5e81840bc3b4e3f1eae2d97b699669306156b
rami-dv <rami.dv99@gmail.com>
2025-06-10 23:15:07 -0400

    add team.ts

---
+++ src/lib/team.ts
@@ -0,0 +1,34 @@
+export default {
+  alex: {
+    name: 'alex',
+    photo: () => import('$images/team/alex.jpg?enhanced')
+  },
+  dawn: {
+    name: 'dawn',
+    photo: () => import('$images/team/dawn.jpg?enhanced')
+  },
+  dorothy: {
+    name: 'dorothy',
+    photo: () => import('$images/team/dorothy.jpg?enhanced')
+  },
+  eli: {
+    name: 'eli',
+    photo: () => import('$images/team/eli.jpg?enhanced')
+  },
+  em: {
+    name: 'em',
+    photo: () => import('$images/team/em.jpg?enhanced')
+  },
+  leo: {
+    name: 'leo',
+    photo: () => import('$images/team/leo.jpg?enhanced')
+  },
+  michel: {
+    name: 'michel',
+    photo: () => import('$images/team/michel.jpg?enhanced')
+  },
+  nick: {
+    name: 'nick',
+    photo: () => import('$images/team/nick.jpg?enhanced')
+  }
+};
