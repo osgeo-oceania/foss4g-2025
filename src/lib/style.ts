fa00d03fc616ef23d3e6fb01dd4139406faeaebb
Pierre K. <OzPerle@users.noreply.github.com>
2025-06-19 19:32:18 +1000

    Update style.ts

--- src/lib/style.ts
+++ src/lib/style.ts
@@ -171,7 +171,7 @@ export default function MapStyle(config: MapConfig): StyleSpecification {
         type: 'line',
         filter: ['has', 'hway_num'],
         paint: {
-          'line-color': '#ac5353', // before: #c4b7ab
+          'line-color': '#b97979', // before: #c4b7ab
           'line-width': [
             'interpolate',
             ['linear'],
