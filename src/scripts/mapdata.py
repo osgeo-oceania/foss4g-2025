67c4c0925223614ab0223cb60c0e100d1b4ea35f
rami-dv <rami.dv99@gmail.com>
2025-06-21 13:53:49 -0400

    add linz-suburbs-and-localities

--- src/scripts/mapdata.py
+++ src/scripts/mapdata.py
@@ -17,6 +17,7 @@ import os
 ##   linz-coastline: https://data.linz.govt.nz/layer/51559-nz-coastlines-and-islands-polygons-topo-1250k/
 ##   linz-road-centrelines: https://data.linz.govt.nz/layer/50329-nz-road-centrelines-topo-150k/
 ##   linz-place-names: https://data.linz.govt.nz/layer/51681-nz-place-names-nzgb/
+##   linz-suburbs=and=localities: https://data.linz.govt.nz/layer/113764-nz-suburbs-and-localities/
 ##   auckland-parks: https://data-aucklandcouncil.opendata.arcgis.com/datasets/3135043373ba48b7a9b5240370cb53ac_0/explore
 ##   auckland-alcohol-control-area: https://data-aucklandcouncil.opendata.arcgis.com/datasets/af300c4cc33b4dff8218394a1eeefe50_0/explore
 
