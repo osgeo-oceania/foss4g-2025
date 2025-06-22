5f045efb27e6355faa18e6d45b3f093e918eb1e9
rami-dv <rami.dv99@gmail.com>
2025-06-21 22:49:45 -0400

    mobile friendly ticket options

--- src/components/TicketOptions.svelte
+++ src/components/TicketOptions.svelte
@@ -17,7 +17,7 @@
     {
       title: 'Sponsor Package',
       description: 'Includes donation to the conference',
-      price: '$299',
+      price: '$699',
       cardClass: 'bg-white border-1 border-primary',
       buttonClass: 'bg-success'
     }
@@ -27,24 +27,28 @@
 <div class="divider divider-primary text-primary my-8 text-4xl">Ticket Options</div>
 
 {#snippet ticketOption(tOption: (typeof ticketOptions)[0])}
-  <div class={`card ${tOption.cardClass} rounded-2xl sm:p-6`}>
-    <div class="mb-3 text-sm uppercase">
-      {tOption.title}
+  <div class={`card ${tOption.cardClass} rounded-2xl p-6`}>
+    <div class="flex sm:flex-col">
+      <div>
+        <div class="mb-3 text-sm uppercase">
+          {tOption.title}
+        </div>
+        <div class="sm:mb-12 text-xs">
+          {tOption.description}
+        </div>
+      </div>
+      <div class="text-2xl flex justify-end sm:justify-normal flex-1">
+        {tOption.price}
+      </div>
     </div>
-    <div class="mb-12 text-xs">
-      {tOption.description}
-    </div>
-    <div class="text-2xl">
-      {tOption.price}
-    </div>
-    <div class="mt-4">
+    <div class="flex sm:justify-normal justify-end mt-4">
       <div class={`btn rounded-full text-sm  font-normal ${tOption.buttonClass}`}>
         Get Your Ticket <span class="icon-[lucide--arrow-up-right] block"></span>
       </div>
     </div>
   </div>
 {/snippet}
-<div class="grid space-x-4 sm:grid-cols-3">
+<div class="grid space-y-4 sm:grid-cols-3 sm:space-x-4">
   {#each ticketOptions as tOption}
     {@render ticketOption(tOption)}
   {/each}
