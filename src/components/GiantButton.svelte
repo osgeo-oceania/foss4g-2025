7bade7e109954b1d2c143c7a3509d39ac8d50f79
rami-dv <rami.dv99@gmail.com>
2025-06-22 17:10:36 -0400

    add GiantButton

---
+++ src/components/GiantButton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  const { children, href } = $props();
+</script>
+
+<div class="flex items-center justify-center">
+  <div class="hidden items-center -space-x-3 sm:flex [&>span]:h-10 [&>span]:w-10">
+    <span class="icon-[material-symbols-light--keyboard-double-arrow-right-rounded]"></span>
+    <span class="icon-[material-symbols-light--keyboard-double-arrow-right-rounded]"></span>
+    <span class="icon-[material-symbols-light--keyboard-double-arrow-right-rounded]"></span>
+    <span class="icon-[material-symbols-light--keyboard-double-arrow-right-rounded]"></span>
+  </div>
+  <a {href} target="_blank" class="btn btn-lg btn-success rounded-full">{@render children()}</a>
+  <div class="hidden items-center -space-x-3 sm:flex [&>span]:h-10 [&>span]:w-10">
+    <span class="icon-[material-symbols-light--keyboard-double-arrow-left-rounded]"></span>
+    <span class="icon-[material-symbols-light--keyboard-double-arrow-left-rounded]"></span>
+    <span class="icon-[material-symbols-light--keyboard-double-arrow-left-rounded]"></span>
+    <span class="icon-[material-symbols-light--keyboard-double-arrow-left-rounded]"></span>
+  </div>
+</div>
