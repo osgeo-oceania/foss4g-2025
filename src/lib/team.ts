28285147e8332249e4bdf2cd461c49aba9b60edb
rami-dv <rami.dv99@gmail.com>
2025-06-22 22:15:31 -0400

    update team info

--- src/lib/team.ts
+++ src/lib/team.ts
@@ -1,34 +1,50 @@
 export default {
   alex: {
-    name: 'alex',
-    photo: () => import('$images/team/alex.jpg?enhanced')
+    name: 'Alex Leith',
+    title: 'Conference Chair',
+    photo: () => import('$images/team/alex.jpg?enhanced'),
+    bio: "Alex is an open geospatial technologist with extensive expertise in software development, cloud infrastructure, and program governance. Throughout his career, Alex's focus has been on making data more easily accessible. By enabling simpler access to data, he believes we can drive positive change and continue to develop sustainably."
   },
   dawn: {
-    name: 'dawn',
-    photo: () => import('$images/team/dawn.jpg?enhanced')
+    name: 'Dawn Hendrick',
+    title: "Communications, Merch",
+    photo: () => import('$images/team/dawn.jpg?enhanced'),
+    bio: "I have more than 40 years of cartographic experience starting my professional career as a geologist/seismic geophysicist. Since moving to Tasmania fourteen years ago I have continued to run my own consultancy DH Geodata Services compiling maps for mining, engineering and environmental companies both in Australia and overseas. I am a retired Spatial Science Teacher from TafeNSW. I have run courses using QGIS for the SSSI (now GCA) and for commercial operations with Alex Leith. I attended the first FOSS4G SotM Oceania conference in Melbourne back in 2018 and have attended every year (except 2022 in Fiji) since then. During COVID I organised the Hobart Hub for 2020 and 2021. I use QGIS software with two volunteer organisations who would not have a mapping capability otherwise."
   },
   dorothy: {
-    name: 'dorothy',
-    photo: () => import('$images/team/dorothy.jpg?enhanced')
+    name: 'Dorothy Pion',
+    title: 'Chair: Travel Grant Program',
+    photo: () => import('$images/team/dorothy.jpg?enhanced'),
+    bio: "Dorothy works in Geological Survey at the Mineral Resources Authority in Papua New Guinea, specialising in GIS analysis, remote sensing, and geological mapping. She is passionate about mentoring and actively participates in the GCA mentoring program each year and is involved in training of new staff at the MRA in QGIS, ensuring everyone stays up to date with the latest advances in GIS/RS technology. Outside of work, she enjoys cooking, reading, and spending time gardening."
   },
   eli: {
-    name: 'eli',
-    photo: () => import('$images/team/eli.jpg?enhanced')
+    name: 'Elisa Puccioni',
+    title: 'Travel Grant Program',
+    photo: () => import('$images/team/eli.jpg?enhanced'),
+    bio: "Elisa is the current chair of OsGeo Oceania board and has been contributing to the community since 2019, serving first as board member, then as Grant committee chair and now as the board chair.<br/><br/>Elisa was born in Italy and moved to New Zealand in 2008, where she currently resides. She has over 18 years of experience in GIS and database administration and has been working with most of the proprietors and open-source GIS packages. She is a great supporter of GIS open-source software as she strongly believes that everybody should be able to know and use GIS, especially people belonging to minority groups or living in remote areas."
   },
   em: {
-    name: 'em',
-    photo: () => import('$images/team/em.jpg?enhanced')
+    name: 'Em Hain',
+    title: "Chair: Program, Website, Volunteers",
+    photo: () => import('$images/team/em.jpg?enhanced'),
+    bio: "Em works at North Road where she assists in the development and integration of QGIS. This is her 3rd year as Program Chair - not planned as she has subbed in for the amazing Simon Nitz who unfortunately needed to step away whilst he got more amazing (#becoming-bionic-man). She is also the contact for QGIS Australia and helps wrangle that group. Herding cats through spreadsheets is her superpower."
   },
   leo: {
-    name: 'leo',
-    photo: () => import('$images/team/leo.jpg?enhanced')
+    name: 'Leo Ghignone',
+    title: "Volunteers, Program, Merch",
+    photo: () => import('$images/team/leo.jpg?enhanced'),
+    bio: "Leo is a Data Engineer with a background in Machine Learning, currently working for the Australian Integrated Marine Observing System to manage data from the seas all around Australia. He loves new challenges and new technologies, and strives to make data open and available to all. He had his first experience with FOSS4G last year, got fascinated by the community and is now trying to contribute to it."
   },
   michel: {
-    name: 'michel',
-    photo: () => import('$images/team/michel.jpg?enhanced')
+    name: 'Michel M Nzikou',
+    title: 'Committee Member',
+    photo: () => import('$images/team/michel.jpg?enhanced'),
+    bio: 'Michel works as a research fellow at the Center of Exploration Targeting (CET), at the University of Western Australia (UWA). He is a data enthusiast with a focus on building QGIS plugins for Geological and Geophysical 3D modelling. A part being an organising committee member, he is the ASEG WA Branch president and part of the MAG 24 committee. Outside work, he enjoys playing volleyball and is also a team member of Fury River Dragon boat for 4 years.'
   },
   nick: {
-    name: 'nick',
-    photo: () => import('$images/team/nick.jpg?enhanced')
+    name: 'Nick Forbes-Smith',
+    title: 'Chair: Sponsorship, Venues/Events, Website',
+    photo: () => import('$images/team/nick.jpg?enhanced'),
+    bio: "Nick is a Technical Leader and Software Engineer specialising in building Web platforms for data visualisation. He focuses on making data accessible and understandable to all, regardless of expertise.<br/><br/>Nick is committed to open data and open-source tools, and actively contributes to these communities. He's driven by projects that create meaningful, positive change."
   }
 };
