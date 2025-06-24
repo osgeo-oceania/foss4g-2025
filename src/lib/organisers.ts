b95820664272b6d51c824af8c4ad446fe02c9fb8
Simon Nitz <62117895+wdcsimon@users.noreply.github.com>
2025-06-25 07:30:33 +1200

    Update organisers.ts

--- src/lib/organisers.ts
+++ src/lib/organisers.ts
@@ -1,33 +1,33 @@
 export default {
   alex: {
     name: 'Alex Leith',
-    title: 'Conference Co-Chair<br />Sponsorship, Finance',
+    title: 'Conference Co-Chair + Sponsorship, Finance',
     photo: () => import('$images/team/alex.jpg?enhanced'),
-    bio: "Alex is an open geospatial technologist with extensive expertise in software development, cloud infrastructure, and program governance. Throughout his career, Alex's focus has been on making data more easily accessible. By enabling simpler access to data, he believes we can drive positive change and continue to develop sustainably."
+    bio: "Alex is an open geospatial technologist; a global leader in the field of Earth observation with a track record in delivering operational data infrastructure. He has extensive expertise in software development, cloud infrastructure, and program governance. Beyond all that, he is a founding Director of OSGeo Oceania and former Chair of the organisation. He enjoys organising small and large events, including as Chair of last year’s FOSS4G SotM Oceania event in Hobart. And... he runs a cloud native geospatial consultancy = https://auspatious.com/"
   },
   simon: {
     name: 'Simon Nitz',
-    title: 'Conference Co-Chair<br />Program, Finance',
+    title: 'Conference Co-Chair + Program, Finance',
     photo: () => import('$images/team/simon.jpg?enhanced'),
-    bio: "Simon is the Geospatial Lead at Whanganui District Council, a local government organisation in New Zealand. With 30+ years in the industry, he is highly regarded throughout Australia and New Zealand as a leader in his field. He has provided GIS consulting services to organisations in the region, focusing on FOSS4G desktop and web mapping solutions. For a stand out 16 years, in the 90’s and 00’s, Simon was chair of the MapInfo User Group in New Zealand. More recently, he has been involved in organising FOSS4G SotM Oceania events."
+    bio: "Simon is the Geospatial Lead at Whanganui District Council, a local government organisation in New Zealand. With 35+ years in the industry, he is highly regarded throughout Australia and New Zealand as a leader in his field. He has provided GIS consulting services to organisations in the region, focusing on FOSS4G desktop and web mapping solutions. For a stand out 16 years, in the 90’s and 00’s, Simon was chair of the MapInfo User Group in New Zealand. More recently, he has been involved in organising FOSS4G SotM Oceania events."
+  },
+  eli: {
+    name: 'Elisa Puccioni',
+    title: 'Travel Grant Program',
+    photo: () => import('$images/team/eli.jpg?enhanced'),
+    bio: "Elisa is a Senior Data Manager with over 15 years’ experience as a GIS specialist and database administrator. Originally from Italy, she moved to New Zealand in 2008 and has worked at Kenex Ltd, a GIS consultancy, delivering projects across mineral exploration, renewables, land management, and environmental science. She has been involved with OSGeo Oceania since 2019, serving as Board Member, Chair, and Deputy Chair. During this time, she redesigned and implemented OSGeo Oceania’s current grant scheme and has coordinated the Travel Grant Program for the FOSS4G Oceania conference for the past three years."
   },
   dawn: {
     name: 'Dawn Hendrick',
     title: "Communications, Merch",
     photo: () => import('$images/team/dawn.jpg?enhanced'),
-    bio: "I have more than 40 years of cartographic experience starting my professional career as a geologist/seismic geophysicist. Since moving to Tasmania fourteen years ago I have continued to run my own consultancy DH Geodata Services compiling maps for mining, engineering and environmental companies both in Australia and overseas. I am a retired Spatial Science Teacher from TafeNSW. I have run courses using QGIS for the SSSI (now GCA) and for commercial operations with Alex Leith. I attended the first FOSS4G SotM Oceania conference in Melbourne back in 2018 and have attended every year (except 2022 in Fiji) since then. During COVID I organised the Hobart Hub for 2020 and 2021. I use QGIS software with two volunteer organisations who would not have a mapping capability otherwise."
+    bio: "Starting her professional career as a geologist/seismic geophysicist, Dawn now has more than 40 years of cartographic experience. She runs her own part-time consultancy, DH Geodata Services, compiling maps for mining, engineering and environmental companies both in Australia and overseas. Though based in Tasmania, she was a Spatial Science Teacher with TafeNSW for 5 years, a position she retired from in 2023. She has run courses using QGIS for the Surveying and Spatial Sciences Institute (SSSI), now GeoSpatial Council of Australia (GCA), and for commercial operations with Alex Leith. She uses QGIS software with two volunteer organisations who would not have a mapping capability otherwise. Dawn attended the first FOSS4G SotM Oceania conference in Melbourne back in 2018 and has attended every year (except 2022 in Fiji) since then.  During COVID, she organised the Hobart Hub for 2020 and 2021. In November 2024, Dawn was an Organising Committee member for the Annual FOSS4G SotM Oceania Conference held in Hobart."
   },
   dorothy: {
     name: 'Dorothy Pion',
     title: 'Chair: Travel Grant Program',
     photo: () => import('$images/team/dorothy.jpg?enhanced'),
     bio: "Dorothy works in Geological Survey at the Mineral Resources Authority in Papua New Guinea, specialising in GIS analysis, remote sensing, and geological mapping. She is passionate about mentoring and actively participates in the GCA mentoring program each year and is involved in training of new staff at the MRA in QGIS, ensuring everyone stays up to date with the latest advances in GIS/RS technology. Outside of work, she enjoys cooking, reading, and spending time gardening."
-  },
-  eli: {
-    name: 'Elisa Puccioni',
-    title: 'Travel Grant Program',
-    photo: () => import('$images/team/eli.jpg?enhanced'),
-    bio: "Elisa is the current chair of OsGeo Oceania board and has been contributing to the community since 2019, serving first as board member, then as Grant committee chair and now as the board chair.<br/><br/>Elisa was born in Italy and moved to New Zealand in 2008, where she currently resides. She has over 18 years of experience in GIS and database administration and has been working with most of the proprietors and open-source GIS packages. She is a great supporter of GIS open-source software as she strongly believes that everybody should be able to know and use GIS, especially people belonging to minority groups or living in remote areas."
   },
   leo: {
     name: 'Leo Ghignone',
