f3fc9e5644e59e510e758b5f1db92936d235207b
Stella Blake-Kelly <hello@cartisanmaps.io>
2025-06-29 11:19:42 +1000

    tweak footer styling

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,19 +3,19 @@
   import Button from './Button.svelte';
 </script>
 
-<div class="px-2 pb-2">
+<div class="px-2 pb-8">
   <div class="bg-primary grid rounded-4xl sm:grid-cols-2">
-    <div class="flex flex-col space-y-4 p-8 text-white">
-      <div class="text-lg">FOSS4G Auckland 2025</div>
+    <div class="flex flex-col space-y-6 p-6 sm:p-12 text-white">
+      <div class="text-xl">FOSS4G Auckland 2025</div>
       <div class="grid grid-cols-2">
-        <ul class="space-y-2">
+        <ul class="space-y-3">
           <li><Link href="/attend/call-for-papers">Call For Papers</Link></li>
           <li><Link href="/attend/register">Register</Link></li>
           <li><Link href="/program/outline">Program</Link></li>
           <li><Link href="/sponsorship">Sponsorship</Link></li>
           <li><Link href="/organisers">Organisers</Link></li>
         </ul>
-        <ul class="space-y-2">
+        <ul class="space-y-3">
           <li><Link href="/attend/terms-and-conditions">Terms &amp; Conditions</Link></li>
           <li><Link href="/attend/code-of-conduct">Code Of Conduct</Link></li>
           <li><Link href="/attend/privacy-policy">Privacy Policy</Link></li>
@@ -23,19 +23,19 @@
       </div>
       <div class="mt-8 text-xs">&copy; FOSS4G Oceania - All rights reserved</div>
     </div>
-    <div class="bg-secondary text-primary space-y-4 rounded-4xl p-8">
+    <div class="bg-secondary text-primary space-y-4 rounded-4xl p-6 sm:p-12">
       <div class="text-md mb-2 uppercase">Stay Updated</div>
-      <div class="">Subscribe for event updates</div>
+      <div class="text-sm">Subscribe for event updates</div>
       <Button
         href="https://mailchi.mp/foss4g/mailing-list"
         target="_blank"
         size="sm"
         color="primary"
-        class="w-auto px-6"
+        class="w-auto px-8 bg-primary text-white hover:bg-success hover:text-primary transition-all duration-200 font-light !bg-primary !text-white hover:!bg-success hover:!text-primary !font-light !py-6"
       >
         Join our Mailing List
       </Button>
-      <div class="text-md pt-4 uppercase">Follow Us</div>
+      <div class="text-md pt-8 uppercase">Follow Us</div>
       <div class=" flex space-x-2 text-2xl uppercase">
         <Link href="https://www.facebook.com/foss4g2025/"
           ><span class="icon-[uil--facebook] block"></span></Link
