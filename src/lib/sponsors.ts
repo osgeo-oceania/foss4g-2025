29f4cf0dcac28c01905240e06cfafe21b91ad0f7
rami-dv <rami.dv99@gmail.com>
2025-06-29 21:05:42 -0400

    add sponsor links

--- src/lib/sponsors.ts
+++ src/lib/sponsors.ts
@@ -10,57 +10,76 @@ export const LEVELS = {
 };
 
 export default {
-  aut: { name: 'AUT', logo: () => import('$images/logos/aut.png?enhanced'), level: LEVELS.VENUE },
+  aut: {
+    name: 'AUT',
+    logo: () => import('$images/logos/aut.png?enhanced'),
+    link: 'https://www.aut.ac.nz/',
+    level: LEVELS.VENUE
+  },
   geocat: {
     name: 'GeoCAT',
     logo: () => import('$images/logos/geocat.png?enhanced'),
+    link: 'https://www.geocat.net/',
     level: LEVELS.GOLD
   },
-  linz: { name: 'LINZ', logo: () => import('$images/logos/linz.png?enhanced'), level: LEVELS.GOLD },
+  linz: {
+    name: 'LINZ',
+    logo: () => import('$images/logos/linz.png?enhanced'),
+    link: 'https://www.linz.govt.nz/',
+    level: LEVELS.GOLD
+  },
   'newzealand-com': {
     name: 'newzealand.com',
     logo: () => import('$images/logos/newzealand-com-shared-white.png?enhanced'),
+    link: 'https://www.newzealand.com/',
     level: LEVELS.GOLD
   },
   'osgeo-oceania': {
     name: 'OSGeo Oceania',
     logo: () => import('$images/logos/osgeo-oceania.png?enhanced'),
+    link: 'https://osgeo-oceania.org/',
     level: LEVELS.PARTNER
   },
   osgeo: {
     name: 'OSGeo',
     logo: () => import('$images/logos/osgeo.png?enhanced'),
+    link: 'https://www.osgeo.org/',
     level: LEVELS.PARTNER
   },
   pgrsc: {
     name: 'PGRSC',
     logo: () => import('$images/logos/pgrsc.png?enhanced'),
+    link: 'https://pgrsc.org/',
     level: LEVELS.SUPPORTER
   },
   qfieldcloud: {
     name: 'QFieldCloud',
     logo: () => import('$images/logos/qfieldcloud.png?enhanced'),
+    link: 'https://qfield.cloud/',
     level: LEVELS.SILVER
-  },
-  sparkgeo: {
-    name: 'SparkGeo',
-    logo: () => import('$images/logos/sparkgeo.png?enhanced'),
-    level: LEVELS.SILVER
-  },
-  sparkgeocc: {
-    name: 'SparkGeo',
-    logo: () => import('$images/logos/sparkgeo.png?enhanced'),
-    level: LEVELS.CHILDCARE
   },
   sotm2025: {
     name: 'SOTM2025',
     logo: () => import('$images/logos/sotm2025.png?enhanced'),
+    link: 'https://2025.stateofthemap.org/',
     level: LEVELS.SUPPORTER
+  },
+  sparkgeo: {
+    name: 'SparkGEO',
+    logo: () => import('$images/logos/sparkgeo.png?enhanced'),
+    link: 'https://sparkgeo.com/',
+    level: LEVELS.SILVER + LEVELS.CHILDCARE
   },
   tomtom: {
     name: 'TomTom',
     logo: () => import('$images/logos/tomtom.png?enhanced'),
+    link: 'https://www.tomtom.com/',
     level: LEVELS.BRONZE
   },
-  wdc: { name: 'WDC', logo: () => import('$images/logos/wdc.png?enhanced'), level: LEVELS.PARTNER }
+  wdc: {
+    name: 'WDC',
+    logo: () => import('$images/logos/wdc.png?enhanced'),
+    link: 'https://www.wdc.govt.nz/',
+    level: LEVELS.PARTNER
+  }
 };
