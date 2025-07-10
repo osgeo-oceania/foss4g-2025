1a2958d2dd62622a7edfc5ca3d3e8abcfd70b6a9
Hamish Campbell <hamish.campbell@urbanintelligence.co.nz>
2025-07-10 19:39:57 +1200

    Add historic news items

--- src/lib/news.ts
+++ src/lib/news.ts
@@ -1,7 +1,7 @@
 export default [
   {
     date: '30th June 2025',
-    title: 'Call for Papers closing soon, early bird extended',
+    title: 'Call for Papers closing soon, early bird extended.',
     content: `
 With your help, we are building a dynamic gathering that fosters collaboration and celebrates innovation in the exciting world of Free and Open Source Software for Geospatial.
 
@@ -9,6 +9,26 @@ We have committed to openness and inclusion and are **thrilled to see papers, li
 
 Our Pacific family warmly welcomes you and are truly excited to have this opportunity to share our place with the global FOSS4G community. Let's work together to make FOSS4G 2025 in Auckland a landmark event, let's make this the most diverse congress yet.
     `,
-    link: 'https://mailchi.mp/foss4g/foss4g-2025-auckland-newsletter-10338572?e=b053e3d475'
+    link: 'https://mailchi.mp/foss4g/foss4g-2025-auckland-newsletter-10338572'
+  },
+  {
+    date: '18th June 2025',
+    title: 'Early Bird ends in 10 days. Submission for papers closing soon.',
+    link: 'https://mailchi.mp/foss4g/foss4g-2025-auckland-newsletter-10338317'
+  },
+  {
+    date: '4th June 2025',
+    title: 'Official FOSS4G 2025 Logo - as chosen by you!',
+    link: 'https://mailchi.mp/foss4g/foss4g-2025-auckland-newsletter-10337332'
+  },
+  {
+    date: '9th May 2025',
+    title: 'Meet FOSS4G Committee Members + Early bird registration is open!',
+    link: 'https://mailchi.mp/foss4g/foss4g-2025-auckland-newsletter-10337308'
+  },
+  {
+    date: '31sr March 2025',
+    title: 'Early Bird FOSS4G 2025 Registration is open!',
+    link: 'https://mailchi.mp/foss4g/foss4g-2025-auckland-newsletter-10336672'
   }
 ];
