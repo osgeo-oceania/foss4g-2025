1a736c1e66cd3f4a1e9565909649e42be924f7df
Hamish Campbell <hamish.campbell@urbanintelligence.co.nz>
2025-07-17 17:45:20 +1200

    Adds the foss4g 2025 welcome video to the home page
    Fixes #119

---
+++ src/components/Video.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  let { src = null } = $props();
+</script>
+
+<div class="video-container">
+  <iframe
+    title="YouTube video player"
+    {src}
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    referrerpolicy="strict-origin-when-cross-origin"
+    allowfullscreen
+  ></iframe>
+</div>
+
+<style>
+  .video-container {
+    position: relative;
+    padding-bottom: 56.25%; /* 16:9 aspect ratio */
+    padding-top: 25px;
+    height: 0;
+  }
+  .video-container iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+</style>
