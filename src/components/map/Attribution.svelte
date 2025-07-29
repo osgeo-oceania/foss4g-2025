1537c2b42312a249ec12af0d3704c0c1477a50b6
rami-dv <rami.dv99@gmail.com>
2025-07-29 19:07:53 -0400

    map state as module export

---
+++ src/components/map/Attribution.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { MapState } from '$components/map/Map.svelte';
+  import { getContext } from 'svelte';
+
+  const mapState = getContext<MapState>('mapState');
+  const mapAttribution = $derived([
+      ...new Set(
+        Object.values(mapState.mapStyle.sources)
+          .filter((src) => src.type != 'video' && src.type != 'image')
+          .map((src) => src?.attribution?.split(', ') ?? null)
+          .flat()
+      )
+    ]);
+</script>
+
+<div class="absolute right-0 bottom-0 z-50 rounded-tl bg-white px-1 pt-0.5 text-xs wrap">
+  <div class="inline-block">map data &copy;</div>
+  <div class="inline-block space-x-0.5">
+    {#each mapAttribution as attribution}
+      <div class="inline-block">{@html attribution}</div>
+    {/each}
+  </div>
+</div>
