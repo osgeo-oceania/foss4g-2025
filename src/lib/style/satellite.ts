1622e91287b7344a8bda18d048b3c9ef5975b4b2
rami-dv <rami.dv99@gmail.com>
2025-08-19 15:37:30 -0400

    add satellite style

---
+++ src/lib/style/satellite.ts
@@ -0,0 +1,36 @@
+import type { StyleSpecification } from 'maplibre-gl';
+import RamiStyle from './rami';
+
+export default {
+  name: 'satellite',
+  style: (config: MapConfig): StyleSpecification => {
+    const rami = RamiStyle.style(config);
+
+    rami.sources['satellite'] = {
+      type: 'raster',
+      tiles: [
+        'https://tiles-cdn.koordinates.com/services;key=95fa766262344182bbf4b67d91b59c3c/tiles/v4/layer=121752/EPSG:3857/{z}/{x}/{y}.png'
+      ],
+      tileSize: 128
+    };
+
+    rami.layers = rami.layers
+      .map((lyr) => {
+        if (lyr.type == 'symbol') return lyr;
+        else if (lyr.type == 'line') {
+          // @ts-ignore
+          lyr.paint['line-opacity'] = 0.6;
+          
+          return lyr;
+        } else return null;
+      })
+      .filter((lyr) => lyr !== null);
+
+    rami.layers.unshift({
+      id: 'satellite',
+      type: 'raster',
+      source: 'satellite'
+    });
+    return rami;
+  }
+};
