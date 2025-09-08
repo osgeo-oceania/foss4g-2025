cf6c7fb74ada766928974b0a9fea4845db7aa3db
rami-dv <rami.dv99@gmail.com>
2025-09-08 18:01:06 -0400

    refactor keynotes to look better on mobile

--- src/components/Keynotes.svelte
+++ src/components/Keynotes.svelte
@@ -3,11 +3,17 @@
 </script>
 
 {#snippet keynoteGrid(keynote: (typeof keynotes)[keyof typeof keynotes], i: number)}
-  <div class="grid h-[240px] sm:h-[300px] sm:grid-cols-2" class:reverse-grid={i % 4 >= 2}>
-    <div class="hidden p-2 text-white sm:flex">
+  <div
+    class="grid h-[240px] grid-cols-2 sm:h-[300px]"
+    class:reverse-grid={i % 2 == 0}
+    class:sm:reverse-grid={i % 4 >= 2}
+  >
+    <div class="flex p-2 text-white sm:flex">
       <div class="bg-primary flex h-full flex-1 flex-col rounded-4xl p-4">
-        <div class="flex-grow text-xl">{keynote.name}</div>
-        <div class="flex-shrink text-xs">president,<br />{keynote.name} inc</div>
+        <div class="flex-shrink text-xl">{keynote.name}</div>
+        <div class="flex-shrink text-xs">{keynote.title}</div>
+        <hr class="border-t border-white my-2 h-[1px]" />
+        <div class="flex-grow text-xs">texto</div>
       </div>
     </div>
     <div class="relative h-full flex-1 p-2">
@@ -21,32 +27,36 @@
           <div
             class="bg-secondary absolute inset-0 flex flex-col opacity-30 mix-blend-multiply"
           ></div>
-          <div class="text-primary absolute z-10 flex h-full w-full flex-col justify-end sm:hidden">
-            <div class=" bg-white/80 p-1">
-              <div class="">{keynote.name}</div>
-              <div class="flex-shrink text-xs">president, {keynote.name} inc</div>
-            </div>
-          </div>
         </div>
       {/await}
     </div>
   </div>
 {/snippet}
 
-<div class="grid grid-cols-2">
+<div class="grid grid-cols-1 sm:grid-cols-2">
   {#each Object.values(keynotes) as keynote, i}
     {@render keynoteGrid(keynote, i)}
   {/each}
 </div>
 
 <style>
-  .reverse-grid {
-    direction: rtl;
+  @media (min-width: 640px) {
+    .sm\:reverse-grid {
+      direction: rtl;
+    }
+    .sm\:reverse-grid > * {
+      direction: ltr;
+    }
+  }
+  @media (max-width: 639px) {
+    .reverse-grid {
+      direction: rtl;
+    }
+    .reverse-grid > * {
+      direction: ltr;
+    }
   }
 
-  .reverse-grid > * {
-    direction: ltr;
-  }
   .harmonized-logo {
     filter: grayscale(100%);
   }
