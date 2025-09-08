ee8ac0573a1f13d541e0e755c393bf3b84fab2ee
rami-dv <rami.dv99@gmail.com>
2025-09-08 17:38:24 -0400

    update keynotes images

--- src/lib/keynotes.ts
+++ src/lib/keynotes.ts
@@ -1,34 +1,18 @@
 export default {
   alex: {
-    name: 'alex',
-    photo: () => import('$images/team/alex.jpg?enhanced')
+    name: 'Amy Rose',
+    photo: () => import('$images/keynotes/amy-rose.jpg?enhanced')
   },
   dawn: {
-    name: 'dawn',
-    photo: () => import('$images/team/dawn.jpg?enhanced')
+    name: 'Ana Belgun',
+    photo: () => import('$images/keynotes/ana-belgun.jpg?enhanced')
   },
   dorothy: {
-    name: 'dorothy',
-    photo: () => import('$images/team/dorothy.jpg?enhanced')
+    name: 'Hirini Tane',
+    photo: () => import('$images/keynotes/hirini-tane.jpg?enhanced')
   },
   eli: {
-    name: 'eli',
-    photo: () => import('$images/team/eli.jpg?enhanced')
+    name: 'Selene Yang',
+    photo: () => import('$images/keynotes/selene-yang.jpg?enhanced')
   },
-  em: {
-    name: 'em',
-    photo: () => import('$images/team/em.jpg?enhanced')
-  },
-  leo: {
-    name: 'leo',
-    photo: () => import('$images/team/leo.jpg?enhanced')
-  },
-  michel: {
-    name: 'michel',
-    photo: () => import('$images/team/michel.jpg?enhanced')
-  },
-  nick: {
-    name: 'nick',
-    photo: () => import('$images/team/nick.jpg?enhanced')
-  }
 };
