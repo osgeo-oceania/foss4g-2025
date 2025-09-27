d5aa60357354f2ad1760a9441769aa63bcef00f2
rami-dv <rami.dv99@gmail.com>
2025-09-26 21:48:35 -0400

    attribution

--- src/components/map/Attribution.svelte
+++ src/components/map/Attribution.svelte
@@ -4,19 +4,22 @@
 
   const mapState = getContext<() => MapState>('mapState')();
   const mapAttribution = $derived([
-      ...new Set(
-        Object.values(mapState.mapStyle.sources)
-          .filter((src) => src.type != 'video' && src.type != 'image')
-          .map((src) => src?.attribution?.split(', ') ?? null)
-          .flat()
-      )
-    ]);
+    ...new Set(
+      Object.values(mapState.mapStyle.sources)
+        .filter((src) => src.type != 'video' && src.type != 'image')
+        .map((src) => src?.attribution?.split(', ') ?? null)
+        .flat()
+        .filter((src) => src !== null)
+    )
+  ]);
 </script>
 
-<div class="absolute right-0 bottom-0 z-30 rounded-tl bg-white px-1 pt-0.5 text-xs wrap">
-  <div class="inline-block">map data &copy;</div>
+<div
+  class="wrap absolute right-0 bottom-0 z-30 rounded-tl bg-white px-1 font-sans text-[9px] opacity-70"
+>
+  <div class="inline-block">&copy;</div>
   <div class="inline-block space-x-0.5">
-    {#each mapAttribution as attribution}
+    {#each mapAttribution as attribution, idx}
       <div class="inline-block">{@html attribution}</div>
     {/each}
   </div>
