9942a2f00419c68525df99506b2a9de4841a85f8
rami-dv <rami.dv99@gmail.com>
2025-11-03 18:13:55 -0500

    hack for localhost .pmtiles.gz

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [enhancedImages(), tailwindcss(), sveltekit()],
+  plugins: [enhancedImages(), tailwindcss(), sveltekit(), {
+    name: 'configure-server',
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        if (req.url?.endsWith('.pmtiles.gz')) {
+          res.setHeader('Content-Encoding', 'identity')
+        }
+        next()
+      })
+    }
+  }],
   assetsInclude: ['src/data/*.glb', 'src/data/*.pmtiles*', 'src/data/glyphs/**/*.pbf']
 });
